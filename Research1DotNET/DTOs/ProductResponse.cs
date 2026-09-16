18f35d76cb0228caf49b438981985ce2046a8ba1
Ahmed Emish <ahmedemish04@gmail.com>
2026-09-16 10:32:57 +0300

    FirstProject: MinimalApis

---
+++ Research1DotNET/DTOs/ProductResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace ProductApi.MinimalApi.DTOs;
+
+public class ProductResponse
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public decimal Price { get; set; }
+    public DateTime CreatedAtUtc { get; set; }
+}
